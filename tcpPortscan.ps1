eaf7155a3a4261f06845c19bdd1a84ce08a42464
Chris <atnl@protonmail.com>
2019-11-23 04:44:00 -0500

    Update tcpPortscan.ps1

--- tcpPortscan.ps1
+++ tcpPortscan.ps1
@@ -9,3 +9,8 @@
 $ports=(21,22,23,25,139,445,1433);$ip="192.168.1.10";foreach($port in $ports) {try{$sockket=New-object 
 System.Net.Sockets.TCPClient($ip,$port); }catch{} ;if($socket -eq $NULL) {echo $ip":"$port" -
 Closed"; } else{echo$ip":"$port" -Open";$socket=$NULL;}}
+
+
+
+
+
